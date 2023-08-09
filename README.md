8e55788ab4f716d43621a7fa147d7460bae9fb84
Caisense <caisense0823@gmail.com>
2023-08-09 21:12:57 +0800

    8-9

--- README.md
+++ README.md
@@ -7,11 +7,11 @@ actionText: 开始 →
 actionLink: /Java基础.html
 features:
 - title: 计算机基础
-  details: Java、网络、数据库、Spring、分布式等（八股）
+  details: 数据库、网络、Java、Spring、分布式等（八股）
 - title: 算法
   details: Leetcode、牛客网等
-- title: For Me
-  details: Self Redemption, 2023.8.2.
+- title: About Me
+  details: For Self Redemption, 2023.8.2.
 footer: Copyright © 2023-present Caisense
 ---
 
--- config.js
+++ config.js
@@ -8,6 +8,7 @@ module.exports = {
     // plugins: ["demo-container"],
 
     themeConfig: {
+        // 导航栏
         nav: [
             {text: 'Home', link: '/'},
             {
@@ -49,6 +50,7 @@ module.exports = {
                     {text: 'RPC', link: '/RPC'}
                 ]
             },
+            {text: '算法', link: '/算法'},
             {
                 text: '其他', items: [
                     {text: '设计模式', link: '/设计模式'},
@@ -57,10 +59,34 @@ module.exports = {
                     {text: '信息安全', link: '/信息安全'}
                 ]
             },
+            {
+                text: '项目', items: [
+                    {text: '高性能数字货币交易系统', link: '/高性能数字货币交易系统'},
+                ]
+            }
         ],
+        // 侧边栏：自动生成
         sidebar: 'auto',
         logo: "images/hero.png",
         lastUpdated: 'Last Updated', // string | boolean
+        // ----------------------git---------------------
+        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: 'caisense/CS-Note',
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        repoLabel: '查看源码',
 
+        // 以下为可选的编辑链接选项
+
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        // docsRepo: 'vuejs/vuepress',
+        // 假如文档不是放在仓库的根目录下：
+        // docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        // docsBranch: 'master',
+        // 默认是 false, 设置为 true 来启用
+        // editLinks: true,
+        // 默认为 "Edit this page"
+        editLinkText: '帮助我们改善此页面！'
     }
 }
