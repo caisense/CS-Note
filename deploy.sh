b23f5658fa9e3bf7491f6d9fcaa593b656968e2f
Caisense <caisense0823@gmail.com>
2024-11-17 22:05:19 +0800

    使用ssh推送

--- deploy.sh
+++ deploy.sh
@@ -29,9 +29,9 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://github.com/caisense/CS-Note.git master:gh-pages
+#git push -f https://github.com/caisense/CS-Note.git master:gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:caisense/CS-Note.git master:gh-pages
 
 cd -
